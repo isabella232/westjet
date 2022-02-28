362e4b82f7c5f44bcd70be36af1447e8f299898a
David Nuescheler <david@nuescheler.ch>
2022-02-28 13:18:56 -0700

    chore: add fligts block

---
+++ blocks/fligts/fligts.js
@@ -0,0 +1,28 @@
+function setClass(els, className) {
+    els.forEach((el) => el.className = className);
+}
+
+export default async function decorate(block) {
+    const tabs = [...block.children];
+    const div = document.createElement('div');
+    div.className = 'fligts-switcher';
+    div.innerHTML=`
+    <span>Flights From</span>
+    <span>Destination</span>
+    <span>Flights</span>
+    `;
+    const spans = [...div.children];
+    spans.forEach((span, i) => {
+        span.addEventListener('click', () => {
+            setClass(tabs, '');
+            setClass(spans, '');
+            spans[i].className = 'selected';
+            tabs[i].className = 'selected';
+        });
+    })
+
+    spans[0].className = 'selected';
+    tabs[0].className = 'selected';
+
+    block.prepend(div);
+}
